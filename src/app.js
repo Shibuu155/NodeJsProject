dbac9d7d29381a42522aa55c1039f722de298c59
Shubham Thakur <shubhamthakur2920@gmail.com>
2021-04-18 16:33:56 +0530

    updating code

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const hbs  = require('hbs');
-const port = process.env.PORt || 8000;
+const port = process.env.PORT || 8000;
 const path = require('path');
 
 const publicPath = path.join(__dirname,'../public') ;
